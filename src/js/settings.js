cb3ebd3cd36a2645a4664324f52f4270ab7f4ab8
Natrium <1583803226@qq.com>
2024-04-28 00:17:37 +0800

    add unlock fps

--- src/js/settings.js
+++ src/js/settings.js
@@ -13,12 +13,20 @@ const initSettings = async () => {
     switch_unlock_fps.closest('.switchitem').querySelector('.text .desc').innerHTML = '检测中'
     switch_unlock_fps.checked = (await window.fireflyAPI.isFPSUnlocked()).msg == 'unlocked'
     switch_unlock_fps.closest('.switchitem').querySelector('.text .desc').innerHTML = tmp
-    let is_unlocking = false
-    switch_unlock_fps.addEventListener('click', async () => {
-        if (is_unlocking) return
-        is_unlocking = true
-        Alert.info(`正在尝试${switch_unlock_fps.checked ? '' : '取消'}解锁帧率`, '请耐心等待', 5)
-
-        is_unlocking = false
+    switch_unlock_fps.addEventListener('click', async (e) => {
+        switch_unlock_fps.disabled = true
+        Alert.info(`正在尝试${switch_unlock_fps.checked ? '' : '取消'}解锁帧率`, '请耐心等待', 7)
+        ret = await window.fireflyAPI.unlockFPS()
+        if (ret.msg == 'OK') {
+            Alert.success(`${switch_unlock_fps.checked ? '' : '取消'}解锁成功`, switch_unlock_fps.checked ? '游戏中图像设置界面显示异常为正常现象' : '请启动游戏查看', 7)
+        } else if (ret.msg == 'reg not found') {
+            switch_unlock_fps.checked = !switch_unlock_fps.checked
+            Alert.error(`${switch_unlock_fps.checked ? '' : '取消'}解锁失败`, '请先在游戏中修改任意图像设置（使图像质量显示为自定义）并退出游戏重试')
+        }
+        else {
+            switch_unlock_fps.checked = !switch_unlock_fps.checked
+            Alert.error(`${switch_unlock_fps.checked ? '' : '取消'}解锁失败`, ret.msg)
+        }
+        switch_unlock_fps.disabled = false
     })
 }
